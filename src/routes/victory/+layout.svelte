bdd46c7d69dbe47151c21d1096d1736cc2aac2e6
Michael Obubelebra Amachree <amachree9630@gmail.com>
2024-11-20 07:40:52 +0100

    ✨ refactor ♻️(ui): Update sidebar & layout styling
    - Revamp sidebar implementation for improved UX
    - Adjust layout styling in Svelte component
    - Enhance visual consistency across the application

--- src/routes/victory/+layout.svelte
+++ src/routes/victory/+layout.svelte
@@ -1,14 +1,19 @@
 <script lang="ts">
 	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
 	import AppSidebar from '$lib/components/app-sidebar.svelte';
-	import "./app.css";
+	import './app.css';
 	let { children } = $props();
+
+	import { useSidebar } from '$lib/components/ui/sidebar/index.js';
+	const sidebar = useSidebar();
 </script>
 
 <Sidebar.Provider>
 	<AppSidebar />
 	<main>
-		<Sidebar.Trigger />
+		<!-- <div class="bg-[#3F003F] text-white">
+			<Sidebar.Trigger />
+		</div> -->
 		<div class="bg-gradient-to-b from-[#2A092A] to-[#3F003F] p-12 text-white">
 			{@render children?.()}
 		</div>
