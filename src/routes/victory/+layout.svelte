eb795a5e39bf53ab22252a60f3714ad677d82cbd
Michael Obubelebra Amachree <amachree9630@gmail.com>
2024-12-06 03:24:59 +0100

    🔧 chore(dependencies): update bits-ui version
    - bump bits-ui from version 1.0.0-next.57 to 1.0.0-next.63
    - ensure compatibility with latest features and fixes
    
    ✨ feat(sheet): enhance portal props support in sheet component
    
    - add portalProps as a prop to SheetPrimitive.Portal
    - improve customization options for the portal
    
    💄 style(victory layout): cleanup commented code
    
    - remove unused commented-out div to improve readability
    - maintain consistency in main layout structure

--- src/routes/victory/+layout.svelte
+++ src/routes/victory/+layout.svelte
@@ -11,9 +11,9 @@
 <Sidebar.Provider>
 	<AppSidebar />
 	<main>
-		<!-- <div class="bg-[#3F003F] text-white">
+		<div class="bg-[#3F003F] text-white">
 			<Sidebar.Trigger />
-		</div> -->
+		</div>
 		<div class="bg-gradient-to-b from-[#2A092A] to-[#3F003F] p-12 text-white">
 			{@render children?.()}
 		</div>
