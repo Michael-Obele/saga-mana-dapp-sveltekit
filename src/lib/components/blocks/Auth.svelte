5a996af2fcb5841e103815d249468b1c7fa8afd0
Michael Obubelebra Amachree <amachree9630@gmail.com>
2024-12-06 03:52:57 +0100

    ✨ feat(alert-dialog): add alert dialog component
    - implement alert dialog with various subcomponents
    - improve UI interactions and accessibility
    
    ✨ feat(badge): create badge component
    
    - add badge with customizable variants
    - styled for various use cases
    
    ✨ feat(switch): introduce switch component
    
    - create a toggle switch with state management
    - enhance form controls interaction and design
    
    ♻️ refactor(auth): remove unused authentication logic
    
    - clean up unnecessary fetchData and handleLogout functions
    - simplify Auth.svelte for better readability
    
    💄 style(Auth): update styles for improved UI
    
    - adjust styling for user avatar and dropdown
    - ensure consistency and enhanced user experience

--- src/lib/components/blocks/Auth.svelte
+++ src/lib/components/blocks/Auth.svelte
@@ -5,30 +5,6 @@
   import Login from './Login.svelte';
   
   let dropdownOpen = false;
-  
-  async function fetchData() {
-    const auth = await authenticate();
-    authStore.set({
-      session: auth.success,
-      username: auth.user?.username || ''
-    });
-  }
-  
-  async function handleLogout() {
-    const result = await logOut();
-    if (result.success) {
-      authStore.set({
-        session: false,
-        username: ''
-      });
-    } else {
-      console.error(result.message);
-    }
-  }
-  
-  onMount(() => {
-    fetchData();
-  });
 </script>
 
 <div>
@@ -40,7 +16,7 @@
           class="bg-none"
         >
           <div class="avatar">
-            <div class="bg-black uppercase h-10 w-10 rounded-full flex items-center justify-center text-white">
+            <div class="flex h-10 w-10 items-center justify-center rounded-full bg-black uppercase text-white">
               {$authStore.username.charAt(0)}
             </div>
           </div>
@@ -51,7 +27,6 @@
             class="absolute right-0 mt-2 w-fit rounded-md bg-popover p-2 shadow-lg"
           >
             <button 
-              on:click={handleLogout}
               class="w-full px-4 py-2 text-left font-bold capitalize hover:bg-gray-100"
             >
               Logout
@@ -65,10 +40,4 @@
   {/if}
 </div>
 
-<style>
-  .avatar {
-    display: inline-flex;
-    align-items: center;
-    justify-content: center;
-  }
-</style>
+
